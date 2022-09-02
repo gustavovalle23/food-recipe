6e147d62a65b2117776891ab9118bcf09827b925
gustavovalle23 <gustavo.vallerp@hotmail.com>
2022-09-02 00:25:14 -0300

    :hammer: moving project to typescript

--- domain/User.js
+++
@@ -1,9 +0,0 @@
-module.exports = class {
-	constructor(id = null, firstName, lastName, email, password) {
-		this.id = id;
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.email = email;
-		this.password = password;
-	}
-};
